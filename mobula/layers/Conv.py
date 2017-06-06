578e8a30382c78addefa6cb277a139c59283155f
Jackie Wu <wkcn@live.cn>
2017-06-06 20:26:38 +0800

    naive Conv layer

--- mobula/layers/Conv.py
+++ mobula/layers/Conv.py
@@ -21,19 +21,40 @@ class Conv(Layer):
         return "It is a Convolution Layer"
     def reshape(self):
         # (NCHW)
-        self.NH = (self.X.shape[2] + self.pad_h * 2 - self.kernel_h) // self.stride
-        self.NW = (self.X.shape[3] + self.pad_w * 2 - self.kernel_w) // self.stride
-        self.Y = np.zeros((self.X.shape[0], self.dim_out, self.NH, self.NW))
+        N,C,H,W = self.X.shape
+        self.NH = (H + self.pad_h * 2 - self.kernel_h + 1) // self.stride
+        self.NW = (W + self.pad_w * 2 - self.kernel_w + 1) // self.stride
+        self.NHW = self.NH * self.NW
+        self.Y = np.zeros((N, self.dim_out, self.NH, self.NW))
         # Convolution Core
         # self.W = Xavier((self.dim_out, self.X.shape[1], self.kernel_h, self.kernel_w)) 
-        self.W = Xavier((self.dim_out, self.X.shape[1] * self.kernel_w * self.kernel_w))
+        self.W = Xavier((self.dim_out, C * self.kernel_w * self.kernel_h))
+        self.b = Xavier((self.dim_out, self.NHW))
+        self.I = im2col(np.pad(np.arange(H * W).reshape((H, W)), ((self.pad_h, self.pad_h), (self.pad_w, self.pad_w)), "constant", constant_values = -1), (self.kernel_h, self.kernel_w), self.stride)
     def get_col(self, X):
-        return np.stack([np.hstack(\
+        return np.stack([np.vstack(\
                 [im2col(\
                 np.pad(X[n,c,:,:], ((self.pad_h, self.pad_h), (self.pad_w, self.pad_w)), "constant"), \
                 (self.kernel_h, self.kernel_w), self.stride) \
-                for c in range(self.X.shape[1])]).T \
-                for n in range(self.X.shape[0])])
+                for c in range(X.shape[1])]) \
+                for n in range(X.shape[0])])
     def forward(self):
         self.X_col = self.get_col(self.X)
-        self.Y = np.dot(self.W, self.X_col).swapaxes(0, 1)
+        self.Y = (np.dot(self.W, self.X_col).swapaxes(0, 1) + self.b).reshape((self.X.shape[0], self.dim_out, self.NH, self.NW))
+    def backward(self):
+        self.dW = np.mean([np.dot(self.dY[i,:,:,:].reshape((self.dim_out, self.NHW)), self.X_col[i,:,:].T) for i in range(self.X.shape[0])], 0)
+        self.db = np.mean(self.dY, 0)
+        dX_col = np.dot(self.W.T, self.dY.reshape(self.X.shape[0], self.dim_out, self.NH * self.NW)).swapaxes(0, 1)
+        self.dX = np.zeros(self.X.shape)
+        N,C,H,W = self.X.shape
+        HW = H * W
+        khw = self.kernel_h * self.kernel_w
+        si = self.I.size
+        self.I.resize(si)
+        for n in range(N):
+            for c in range(C):
+                f = dX_col[c,khw * c:khw * (c+1), :].reshape(si) 
+                np.add.at(self.dX[n,c,:,:].reshape(HW), self.I[self.I != -1], f[self.I != -1])
+    def update(self, lr):
+        self.W -= lr * self.dW
+        self.b -= lr * self.db
--- test/testConv.py
+++ test/testConv.py
@@ -8,12 +8,22 @@ X[0,1,:,:] = np.arange(25, 50).reshape((5,5))
 X[1,0,:,:] = np.arange(125, 150).reshape((5,5))
 
 data = Data(X, "data") 
-conv = Conv(data, "Conv", pad = 0, kernel = 3, dim_out = 2)
+conv = Conv(data, "Conv", pad = 0, kernel_h = 2, kernel_w = 3, dim_out = 3)
 conv.reshape()
 conv.forward()
+print "X", conv.X.shape
+print conv.X_col.shape
+conv.dY = conv.Y
+print conv.W.T.shape, conv.dY.shape
+conv.backward()
+#print X
+print "==="
+#print conv.X_col
+print conv.X_col.shape
+print conv.dX
 '''
 print X
 print conv.X_col.shape
 print conv.W.shape
 '''
-print conv.Y.shape
+#print conv.Y.shape
