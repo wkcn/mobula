b922d53f34e2f07527e9588096fadac9e54a4af7
Jackie Wu <wkcn@live.cn>
2017-06-25 15:48:11 +0800

    Optimize Data Layer

--- mobula/layers/Data.py
+++ mobula/layers/Data.py
@@ -4,48 +4,51 @@ class Data(Layer):
     def __init__(self, model, layer_name = "", *args, **kwargs):
         self.layer_name = layer_name
         self.model = None
-        self.allY = model
+        self._Y = model
         self.dim_out = model.shape[1]
         self.next_layers = []
         self.lr = 0.0
         self.batch_size = kwargs.get("batch_size")
         self.batch_i = 0
         self._label = kwargs.get("label")
-        self.N = len(self.allY)
+        self.N = len(self._Y)
+        self.rounds = 0 
     def __str__(self):
         return "It is a Data Layer"
+    def reset_index(self):
+        self.batch_i = 0
     def set_data(self, Y):
-        self.allY = Y
-        self.N = len(self.allY)
+        self._Y = Y
+        self.rounds = 0
+        self.N = len(self._Y)
     def set_label(self, label):
         self._label = label
     def reshape(self):
         if self.batch_size:
-            yshape = [self.allY.shape[i] for i in range(len(self.allY.shape))]
-            if self.batch_size < yshape[0]:
-                yshape[0] = self.batch_size
-            self.Y = np.zeros(yshape)
-            if self._label is not None:
-                lshape = [self._label.shape[i] for i in range(len(self._label.shape))]
-                if self.batch_size < lshape[0]:
-                    lshape[0] = self.batch_size
-                self.label = np.zeros(lshape).astype(np.int)
-        else:
-            self.Y = self.allY 
-            self.label = self._label
+            self.index = [0] * self.batch_size
     def forward(self):
         if self.batch_size is None:
             return
         i = 0
+        self.index = []
         while i < self.batch_size:
             bi = self.batch_i
             be = bi + self.batch_size - i
             if be >= self.N: 
                 be = self.N
                 self.batch_i = 0
+                self.rounds += 1
             else:
                 self.batch_i = be 
-            j = be - bi + i 
-            self.Y[i:j,:,:,:] = self.allY[bi:be,:,:,:]
-            self.label[i:j, :] = self._label[bi:be, :]
-            i = j
+            self.index.extend([k for k in range(bi, be)])
+            i += (be - bi)
+    @property
+    def Y(self):
+        if self.batch_size is None:
+            return self._Y
+        return self._Y[self.index]
+    @property
+    def label(self):
+        if self.batch_size is None:
+            return self._label
+        return self._label[self.index]
