a75dc9da16fcbb009408092170891a619aea7ca9
wkcn <wkcn@live.cn>
2017-08-07 16:40:57 +0800

    add test for sigmoid and relu

--- mobula/layers/ReLU.py
+++ mobula/layers/ReLU.py
@@ -7,7 +7,7 @@ class ReLU(Layer):
         self.Y = np.zeros(self.X.shape)
     def forward(self):
         self.Y = self.X.copy()
-        self.Y[self.X < 0] = 0.0
+        self.Y[self.X <= 0] = 0.0
     def backward(self):
         self.dX = self.dY.copy()
-        self.dX[self.X < 0] = 0.0
+        self.dX[self.X <= 0] = 0.0
---
+++ tests/test_layers/test_activation.py
@@ -0,0 +1,33 @@
+import mobula.layers as L
+import numpy as np
+
+X = ((np.arange(10000) - 5000) / 1000.0).reshape((-1, 1, 1, 1))
+data = L.Data(X, "data")
+data.reshape()
+
+def test_sigmoid():
+    l = L.Sigmoid(data, "sigmoid")
+    l.forward()
+    l.dY = np.random.random(l.Y.shape) * 10
+    l.backward()
+
+    enx = np.exp(-X)
+    assert np.allclose(l.Y.ravel(), (1.0 / (1.0 + enx)).ravel())
+    assert np.allclose(l.dX.ravel(), (enx / np.square(1 + enx) * l.dY).ravel())
+
+def test_relu():
+    l = L.ReLU(data, "ReLU")
+    l.forward()
+    l.dY = np.random.random(l.Y.shape) * 10
+    l.backward()
+    Y = np.zeros(X.shape)
+    b = (X > 0)
+    Y[b] = X[b]
+    dX = np.zeros(X.shape)
+    dX[b] = l.dY[b]
+    '''
+    d = (l.dX != dX)
+    print (l.dX[d], dX[d])
+    '''
+    assert np.allclose(l.Y.ravel(), Y.ravel())
+    assert np.allclose(l.dX.ravel(), dX.ravel())
