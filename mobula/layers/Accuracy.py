e68e39683d0951b31d6c3d80e1cbd7a6fd1ec3d7
wkcn <wkcn@live.cn>
2017-08-14 10:27:14 +0800

    fix accuracy layer

--- mobula/layers/Accuracy.py
+++ mobula/layers/Accuracy.py
@@ -4,7 +4,7 @@ class Accuracy(LossLayer):
     def __init__(self, model, *args, **kwargs):
         LossLayer.__init__(self, model, *args, **kwargs)
         self.top_k = kwargs.get("top_k", 1)
-    def shape(self):
+    def reshape(self):
         self.Y = 0.0
     def forward(self):
         w = np.argpartition(self.X, -self.top_k, axis = 1)[:, -self.top_k:]
