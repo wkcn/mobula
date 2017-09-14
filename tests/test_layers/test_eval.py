90cb4d87b593e1d5899bcb8eedd6251f1b1852ba
wkcn <wkcn@live.cn>
2017-09-14 10:01:06 +0800

    more tests for Layer.eval()

--- tests/test_layers/test_eval.py
+++ tests/test_layers/test_eval.py
@@ -27,3 +27,32 @@ def test_eval():
     assert np.allclose(r_add.eval(datas), a + b)
     assert np.allclose(r_multiply.eval(datas), np.multiply(a,b))
     assert np.allclose(r_subtract.eval(datas), a - b)
+
+    N,C,H,W = 1,3,2,4
+    a = np.random.random((N, C, H, W))
+    b = np.random.random((N, C, H, W))
+    datas = {
+            data_a: a,
+            data_b: b
+    }
+    assert np.allclose(r_add.eval(datas), a + b)
+    assert np.allclose(r_multiply.eval(datas), np.multiply(a,b))
+    assert np.allclose(r_subtract.eval(datas), a - b)
+
+def test_list_eval():
+    N, C, H, W = 2,3,4,5
+    a = np.random.random((N, C, H, W))
+    b = np.random.random((N, C, H, W))
+    l = L.Data([a,b])
+    [la, lb] = l
+    w = la + lb
+    c = np.random.random((N, C, H, W))
+    d = np.random.random((N, C, H, W))
+    datas = {l:[c, d]}
+    np.allclose(c + d, w.eval(datas))
+
+    N,C,H,W = 1,3,4,2
+    c = np.random.random((N, C, H, W))
+    d = np.random.random((N, C, H, W))
+    datas = {l:[c, d]}
+    np.allclose(c + d, w.eval(datas))
