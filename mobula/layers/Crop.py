b512cacda4113edeab2e831cb453375b30ef006d
wkcn <wkcn@live.cn>
2017-09-17 14:45:36 +0800

    update Crop Layer

--- mobula/layers/Crop.py
+++ mobula/layers/Crop.py
@@ -5,36 +5,18 @@ class Crop(Layer):
         # the type of models is list
         Layer.__init__(self, models[0], *args, **kwargs)
         self.R = models[1]
-        self.axis = kwargs.get("axis", 2)
-        assert 0 <= self.axis < 4
-        self.offset = kwargs.get("offset", 0)
-        if type(self.offset) != list:
-            self.offset = np.ones(4 - self.axis) * self.offset
-        else:
-            assert len(self.offset) == 4 - self.axis, RuntimeError("len(Crop.offset) must be 4 - self.axis")
-        self.offset = np.array(self.offset).astype(np.int)
+        self.axis = kwargs.get("axis", 2) # N,C,H,W
+        self.offset = kwargs.get("offset", 0) # int or list
     def reshape(self):
         self.Y = np.zeros(self.R.shape)
+        n = self.X.ndim - self.axis
+        if type(self.offset) == int:
+            self.axes = [slice(None)] * self.axis + [slice(self.offset, self.offset + self.R.Y.shape[i + self.axis]) for i in range(self.R.Y.ndim - self.axis)]
+        else:
+            assert len(self.offset) == n, "len(Crop.offset) must be the number of dims which will be cropped"
+            self.axes = [slice(None)] * self.axis + [slice(o, o + self.R.Y.shape[i + self.axis]) for i, o in enumerate(self.offset)]
     def forward(self):
-        s = self.R.shape
-        o = self.offset
-        if self.axis == 0:
-            self.Y = self.X[o[0]:o[0]+s[0], o[1]:o[1]+s[1], o[2]:o[2]+s[2], o[3]:o[3]+s[3]] 
-        elif self.axis == 1:
-            self.Y = self.X[:, o[0]:o[0]+s[1], o[1]:o[1]+s[2], o[2]:o[2]+s[3]] 
-        elif self.axis == 2:
-            self.Y = self.X[:, :, o[0]:o[0]+s[2], o[1]:o[1]+s[3]] 
-        else:
-            self.Y = self.X[:, :, :, o[0]:o[0]+s[3]] 
+        self.Y = self.X[self.axes]
     def backward(self):
-        s = self.R.shape
-        o = self.offset
         self.dX = np.zeros(self.X.shape)
-        if self.axis == 0:
-            self.dX[o[0]:o[0]+s[0], o[1]:o[1]+s[1], o[2]:o[2]+s[2], o[3]:o[3]+s[3]] = self.dY
-        elif self.axis == 1:
-            self.dX[:, o[0]:o[0]+s[1], o[1]:o[1]+s[2], o[2]:o[2]+s[3]] = self.dY
-        elif self.axis == 2:
-            self.dX[:, :, o[0]:o[0]+s[2], o[1]:o[1]+s[3]] = self.dY
-        else:
-            self.dX[:, :, :, o[0]:o[0]+s[3]] = self.dY
+        self.dX[self.axes] = self.dY
--- tests/test_layers/test_crop.py
+++ tests/test_layers/test_crop.py
@@ -2,20 +2,35 @@ import mobula.layers as L
 import numpy as np
 
 def test_crop():
-    X1 = np.arange(3 * 4 * 5 * 5).reshape((3,4,5,5))
-    X2 = np.zeros((3,4,3,3))
-    [x1, x2] = L.Data([X1, X2])
-    l = L.Crop([x1, x2], offset = 1, axis = 2)
-    l.reshape()
-    l.forward()
-    target = X1[:,:,1:4,1:4]
-    print (target, l.Y)
-    assert np.allclose(target, l.Y)
-    l.dY = np.arange(l.Y.size).reshape(l.Y.shape)
-    l.backward()
-    tmp = np.zeros(X1.shape)
-    tmp[:,:,1:4,1:4] = l.dY
-    assert np.allclose(tmp, l.dX)
+    X1 = np.arange(4 * 5 * 6 * 6).reshape((4,5,6,6))
+    for offset in range(2): 
+        for axis in range(4):
+            to_shp = list(X1.shape)
+            w = [slice(None)] * 4
+            for i in range(axis, 4):
+                to_shp[i] -= 2
+                w[i] = slice(offset, to_shp[i] + offset)
 
-    l2 = L.Crop([x1, x2], offset = [1,1], axis = 2)
-    assert (l.offset == l2.offset).all()
+            X2 = np.zeros(to_shp)
+            [x1, x2] = L.Data([X1, X2])
+            l = L.Crop([x1, x2], offset = offset, axis = axis)
+            l.reshape()
+            l.forward()
+            assert np.allclose(l.Y, X1[w]) 
+            l.dY = np.random.random(l.Y.shape)
+            l.backward()
+            tmp = np.zeros(X1.shape)
+            tmp[w] = l.dY
+            assert np.allclose(l.dX, tmp)
+
+def test_crop2():
+    X1 = np.arange(3 * 4 * 5 * 5).reshape((3,4,5,5))
+    X2 = np.zeros((2,2,2,2))
+    [x1, x2] = L.Data([X1, X2])
+    l = L.Crop([x1, x2], offset = (1,2,1), axis = 1)
+    assert np.allclose(l.eval(), X1[:, 1:3, 2:4, 1:3])
+    l.dY = np.random.random(l.Y.shape)
+    l.backward()
+    dX = np.zeros(X1.shape) 
+    dX[:,1:3,2:4,1:3] = l.dY
+    assert np.allclose(l.dX, dX)
