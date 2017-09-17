67763d568b4f9ce666631afec679a98d5a5d12ff
wkcn <wkcn@live.cn>
2017-09-17 12:35:12 +0800

    release mobula 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'mobula',
-        version = '1.0.0',
+        version = '1.0.1',
         description = 'A Lightweight & Flexible Deep Learning (Neural Network) Framework in Python',
         author = 'wkcn',
         author_email = 'wkcn@live.cn',
