2b84e8e2c5a4ffe49f574412885f9dde359c0e2e
JackieWu <wkcn@live.cn>
2017-09-23 15:20:03 +0800

    add symbol test

--- tests/test_ops/test_ops.py
+++ tests/test_ops/test_ops.py
@@ -1,5 +1,7 @@
 import mobula as M
+import mobula.layers as L
 import numpy as np
+import operator
 
 def test_ops():
     N, C, H, W = 2,3,4,5
@@ -20,3 +22,21 @@ def test_ops():
     b = np.random.random((N, K))
     assert np.allclose(M.matmul(a, b).eval(), np.dot(a, b))
     assert np.allclose(M.dot(a, b).eval(), np.dot(a, b))
+
+def test_symbols():
+    N, C, H, W = 2,3,4,5
+    a = np.random.random((N, C, H, W))
+    b = np.random.random((N, C, H, W))
+    [la, lb] = L.Data([a,b])
+    eq = M.equal(la, lb)
+    ne = M.not_equal(la, lb)
+    ge = M.greater_equal(la, lb)
+    gt = M.greater(la, lb)
+    le = M.less_equal(la, lb)
+    lt = M.less(la, lb)
+ 
+    lops = [eq,ne,ge,gt,le,lt]
+    ops = [operator.eq, operator.ne, operator.ge, operator.gt, operator.le, operator.lt]
+    for l, op in zip(lops, ops):
+        assert l.op == op
+
