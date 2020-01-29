fix the backward of softmax layer

mobula/layers/Softmax.py
@@ -11,4 +11,42 @@ class Softmax(Layer):
         s = np.sum(e, axis = self.axis, keepdims = True)
         self.Y = e / s
     def backward(self):
-        self.dX = np.multiply(self.dY, self.Y - np.square(self.Y))
+        shape = self.X.shape
+        # (N, C, P)
+        N = int(np.prod(shape[:self.axis]))
+        C = int(shape[self.axis])
+        P = int(np.prod(shape[self.axis+1:]))
+
+        '''
+        dX = np.zeros((N, C, P))
+        y = self.Y.reshape((N, C, P))
+        dY = self.dY.reshape((N, C, P))
+        for n in range(N):
+            for p in range(P):
+                # case: (n, p)
+                for i in range(C):
+                    for j in range(C):
+                        if i == j:
+                            dX[n, i, p] += (y[n, i, p] - y[n, i, p] ** 2) * dY[n, j, p]
+                        else:
+                            dX[n, i, p] += (-y[n, i, p] * y[n, j, p]) * dY[n, j, p]
+        self.dX = dX.reshape(self.X.shape)
+        '''
+
+        y1 = self.Y.reshape((N, C, 1, P))
+        y2 = self.Y.reshape((N, 1, C, P))
+
+        s = (-y1) * y2
+        # inds in (N, C, C, P)
+        batch_inds = np.arange(0, (C*C*P)*N, C*C*P) # N
+        # (n, 0, 0, k) -> (n, 1, 1, k) -> (n, 2, 2, k)
+        diag_inds = np.arange(0, (P+C*P)*C, P+C*P) # C
+        spatial_inds = np.arange(0, P) # P
+        diag_inds = np.repeat(diag_inds, P)
+        diag_inds += np.tile(spatial_inds, C)
+        inds = np.repeat(batch_inds, C*P) + np.tile(diag_inds, N)
+        # (N, C, C, P), self.Y: P->C->N
+        s.ravel()[inds] += self.Y.ravel()
+        # self.dY: (N, C, P)
+        s *= self.dY.reshape(y2.shape)
+        self.dX = s.sum(2).reshape(self.X.shape)

tests/test_layers/test_softmax.py
@@ -1,4 +1,5 @@
 import mobula.layers as L
+from mobula.testing import gradcheck
 from mobula.layers.utils.Defines import *
 import numpy as np
 
@@ -33,8 +34,6 @@ def test_softmax():
         # softmax backward
         l.dY = np.random.random(l.Y.shape)
         l.backward()
-        dX = np.multiply(exp / s - np.square(exp / s), l.dY)
-        assert np.allclose(l.dX, dX)
         # softmax-with-loss backward
         loss_l.dY = np.random.random(loss_l.Y.shape)
         loss_l.backward()
@@ -42,3 +41,10 @@ def test_softmax():
         z.ravel()[get_idx_from_arg(z, label, axis)] = 1
         tl = y - z 
         assert np.allclose(tl * loss_l.dY, loss_l.dX)
+
+def test_softmax_grad():
+    N, C, H, W = 2, 3, 4, 5
+    a = np.random.random((N, C, H, W)) - 0.5
+    for axis in range(4):
+        l = L.Softmax(a, axis = axis)
+        gradcheck(l, a)