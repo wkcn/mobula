48f97c434815b024b4f29f66f6801be1c6f4739a
muaddibusulll <taha@elzo.ai>
2024-06-16 02:06:30 +0530

    first commit

--- mobula/layers/CrossEntropy.py
+++ mobula/layers/CrossEntropy.py
@@ -1,12 +1,16 @@
 from .LossLayer import *
+import numpy as np
 
 class CrossEntropy(LossLayer):
     def __init__(self, model, *args, **kwargs):
-        LossLayer.__init__(self, model, *args, **kwargs)
+        super().__init__(model, *args, **kwargs)
+
     def reshape(self):
         self.Y = 0.0 
+
     def forward(self):
-        self.Y = np.mean(- np.multiply(self.label, np.log(self.X)) - \
-               np.multiply(1.0 - self.label, np.log(1.0 - self.X)))
+        self.Y = np.mean(- np.multiply(self.label, np.log(self.X + 1e-15)) - \
+               np.multiply(1.0 - self.label, np.log(1.0 - self.X + 1e-15)))
+
     def backward(self):
-        self.dX = (-self.label / self.X + (1.0 - self.label) / (1.0 - self.X)) * self.dY
+        self.dX = (-self.label / (self.X + 1e-15) + (1.0 - self.label) / (1.0 - self.X + 1e-15)) * self.dY
